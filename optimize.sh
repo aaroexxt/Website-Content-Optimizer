b8d8958925cac4a323bfe0a34db6db37afdba46a
Aaron Becker <aaron.becker@gmail.com>
2023-10-13 01:21:17 -0400

    ghostscript preserve annotations on PDFs

--- optimize.sh
+++ optimize.sh
@@ -114,9 +114,8 @@ containsElement () {
 }
 
 # thanks https://gist.github.com/ahmed-musallam/27de7d7c5ac68ecbd1ed65b6b48416f9
-pdfcompress()
-{
-   gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$2 $1; 
+pdfcompress() {
+   gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dPreserveAnnots=true -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$2" "$1"; 
 }
 
 # Various counters for operations
